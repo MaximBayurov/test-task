c52a9e4b11052325034f150d90bbde56875ee3e7
Tirnous <nikita.gmail.com>
2019-09-20 22:00:02 +0300

    Привет всем кто первый день на крокодиле

--- src/com/company/Mondelbrot.java
+++ src/com/company/Mondelbrot.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Mondelbrot {
 
     static int BAILOUT = 16;
-    static int MAX_ITERATIONS = 1000;
+    static int MAX_ITERATIONS = 1000000;
     static IsPoint[][] arr = new IsPoint[80][80];
 
     private static int iterate(float x, float y) {
@@ -58,53 +58,93 @@ public class Mondelbrot {
         Date d2 = new Date();
         long diff = d2.getTime() - d1.getTime();
         System.out.println("\nJava Elapsed " + diff/1000.0f);
-        float outside = 0, inside = 0, porebrik = 0;
-        int counterOutside = 0, counterInside = 0, counterPorebrik = 0;
-        int[][] porebrikAria = new int[293][2];
-        int fuck = 0;
+        //Тут начинаются тёрки с подсчётом времени каждого типа
+        ArrayList<Integer[]> empty = new ArrayList<Integer[]>();
+        ArrayList<Integer[]> border = new ArrayList<Integer[]>();
+        ArrayList<Integer[]> filling = new ArrayList<Integer[]>();
         for (int i = 0; i < 80; i++) {
             for (int j = 0; j < 80; j++) {
+                Integer[] coords = new Integer[2];
                 try {
                     if (!arr[i][j].star) {
-                        outside += arr[i][j].secs;
-                        counterOutside++;
+                        coords = new Integer[2];
+                        coords[0] = i;
+                        coords[1] = j;
+                        empty.add(coords);
                     } else if (!arr[i + 1][j].star || !arr[i - 1][j].star || !arr[i][j + 1].star || !arr[i][j - 1].star) {
-                        porebrik += arr[i][j].secs;
-                        counterPorebrik++;
-                        porebrikAria[fuck][0] = i;
-                        porebrikAria[fuck][1] = j;
-                        fuck++;
+                        coords = new Integer[2];
+                        coords[0] = i;
+                        coords[1] = j;
+                        border.add(coords);
                     } else {
-                        inside += arr[i][j].secs;
-                        counterInside++;
+                        coords = new Integer[2];
+                        coords[0] = i;
+                        coords[1] = j;
+                        filling.add(coords);
                     }
                 } catch (IndexOutOfBoundsException err) {
-                    porebrik += arr[i][j].secs;
-                    counterPorebrik++;
+                    coords[0] = i;
+                    coords[1] = j;
+                    border.add(coords);
                 }
             }
         }
+//        empty.forEach((e) -> System.out.print(e[0] + "" + e[1]) ); // просто интересная штука
+        int sizeEmpty = empty.size();
+        Integer[][] emptyDots = new Integer[sizeEmpty][2]; // Создаём массив размера динамического листа
+        empty.toArray(emptyDots);                             // Кладём туда инфу из динамчиеского листа
+        // Всё это делали для того, чтобы не выводить из динамического листа, т.к. он выводит вроде бы менее охотно
+        Date dateStart = new Date(); // !!! Можно сделать функцию и вызвать её три раза !!!
+        for (Integer[] element : emptyDots) {
+            iterate(element[0], element[1]); // Игнорирование вывода, подсчёт времени после нахождения групп,
+        }                                    //а также преобразование динамического листа - идёт ради чистоты времени
+        Date dateFinish = new Date();
+        long differenceEmpty = dateFinish.getTime() - dateStart.getTime();
 
-        System.out.println("Outside = " + outside);
-        System.out.println("Porebrik = " + porebrik);
-        System.out.println("Inside = " + inside);
-        System.out.println("Srednee Outside = " + outside/counterOutside);
-        System.out.println("Srednee Porebrik = " + porebrik/counterPorebrik);
-        System.out.println("Srednee Inside = " + inside/counterInside);
-
-        System.out.println("counterPorebrik: " + counterPorebrik);
-
-        for (int i = 0; i < 80; i++) {
-            for (int j = 0; j < 80; j++) {
-                boolean r = false;
-                for (int k = 0; k < 293; k++) {
-                    if (porebrikAria[k][0] == i && porebrikAria[k][1] == j) r = true;
-                }
-                if (r) System.out.print("*");
-                else System.out.print(" ");
-            }
-            System.out.println();
+        int sizeBorder = border.size();
+        Integer[][] borderDots = new Integer[sizeBorder][2];
+        border.toArray(borderDots);
+        dateStart = new Date();
+        for (Integer[] element : emptyDots) {
+            iterate(element[0], element[1]);
         }
+        dateFinish = new Date();
+        long differenceBorder = dateFinish.getTime() - dateStart.getTime();
+
+        int sizeFilling = filling.size();
+        Integer[][] fillingDots = new Integer[sizeFilling][2];
+        border.toArray(fillingDots);
+        dateStart = new Date();
+        for (Integer[] element : emptyDots) {
+            iterate(element[0], element[1]);
+        }
+        dateFinish = new Date();
+        long differenceFilling = dateFinish.getTime() - dateStart.getTime();
+        // Всё что идёт на выход ниже, пойдёт в графики, я думаю нужно и общее время областей(оно кстати очень нестабильно) и среднее время точки
+        //по факту эти графики имеют одинаковые оси, просто масштаб по у немного иной, но в целом идентичны
+        System.out.println("Все точки вне фигуры: " + differenceEmpty + "мс.\nВсе точки на границе: " + differenceBorder + "мс.\nВсе точки внутри фигуры: " + differenceFilling + "мс.");
+        System.out.print("Средняя точка вне фигуры: " + differenceEmpty / (double)sizeEmpty + "мс.\nСредняя точка на границе: " + differenceBorder / (double)sizeBorder + "мс.\nСредняя точка внутри фигуры: " + differenceFilling / (double)sizeFilling + "мс.");
+
+//        System.out.println("Outside = " + outside); // Вывод точек на границе, вдруг понадобится? Там правда теперь надо много менять
+//        System.out.println("Porebrik = " + porebrik);
+//        System.out.println("Inside = " + inside);
+//        System.out.println("Srednee Outside = " + outside/counterOutside);
+//        System.out.println("Srednee Porebrik = " + porebrik/counterPorebrik);
+//        System.out.println("Srednee Inside = " + inside/counterInside);
+//
+//        System.out.println("counterPorebrik: " + counterPorebrik);
+//
+//        for (int i = 0; i < 80; i++) {
+//            for (int j = 0; j < 80; j++) {
+//                boolean r = false;
+//                for (int k = 0; k < 293; k++) {
+//                    if (porebrikAria[k][0] == i && porebrikAria[k][1] == j) r = true;
+//                }
+//                if (r) System.out.print("*");
+//                else System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
     }
 }
 
