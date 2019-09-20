23635b65ff49269c668098734d83a5957a1f1c82
Maxim Bayurov <vakakak20@gmail.com>
2019-09-20 23:32:17 +0300

    класс gui и сигнатуры его методов

--- src/com/company/gui.java
+++ src/com/company/gui.java
@@ -1,4 +1,28 @@
 package com.company;
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class gui {
+    double[] series1=new double[5];
+    double[] series2=new double[5];
+    double[] series3=new double[5];
+
+    protected gui(){
+
+    }
+
+    public static void setSeries(double[] dataSeries){
+
+    }
+
+    public static void draw(){
+
+    }
+
 }
